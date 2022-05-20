43006871eb8ff6014d30a8dbb7b6501cd05c5d64
Floris <58078572+Flor0s@users.noreply.github.com>
2022-05-20 09:59:39 +0200

    push usland

---
+++ TabletTest/Assets/Scripts/ObjectLevelUp.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLevelUp : MonoBehaviour
+{
+    [Header("levels")]
+    public GameObject[] Diffrentstages;
+
+    public bool DoLevelUp = false;
+    [SerializeField] private int Level = 0;
+
+    private void Start()
+    {
+        Level = 0;
+        Diffrentstages[Level].gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (DoLevelUp == true) //transition toevoegen iets van particles en the jumping via code
+        {
+            DoLevelUp = false;
+            levelUP();
+        }
+    }
+
+    private void levelUP()
+    {
+        Diffrentstages[Level].gameObject.SetActive(false);
+        Level++;
+        Diffrentstages[Level].gameObject.SetActive(true);
+    }
+}
