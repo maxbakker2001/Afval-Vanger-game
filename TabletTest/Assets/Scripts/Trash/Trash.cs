ec1b5c0cf9af73f71d138fc8872154de94a474bf
maxbakker2001 <max.bakker4006@gmail.com>
2022-05-24 13:49:18 +0200

    trash

--- TabletTest/Assets/Scripts/Trash/Trash.cs
+++ TabletTest/Assets/Scripts/Trash/Trash.cs
@@ -1,24 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Pool;
-using Random = UnityEngine.Random;
 
 public class Trash : MonoBehaviour
-{
-    private Action<Trash> _KillAction;
-    public UnityEvent Trashgevangen;
 
-    public void Init(Action<Trash> killAction)
+{
+
+    private void OnCollisionEnter(Collision collision)
     {
-        _KillAction = killAction;
+        Destroy(gameObject);
     }
 
-    private void OnCollisionEnter(Collision col)
+    private void Update()
     {
-        Trashgevangen.Invoke();
-        if (col.transform.CompareTag("Netjes")) _KillAction(this);
+        transform.Translate(0, -1 * Time.deltaTime, 0);
     }
 }
--- TabletTest/Assets/Scripts/Trash/TrashSpawner.cs
+++ TabletTest/Assets/Scripts/Trash/TrashSpawner.cs
@@ -7,45 +7,26 @@ using Random = UnityEngine.Random;
 
 public class TrashSpawner : MonoBehaviour
 {
-    private ObjectPool<Trash> _pool;
 
     [Header("References")]
     [SerializeField] private Trash trashPrefab;
 
     [SerializeField] private SpawnPoint[] spawnPoints;
 
-    [Header("Variables")]
-    [SerializeField] private int trashCount = 10;
 
-    [SerializeField] private int maxTrash = 20;
     [SerializeField] private int spawnFrequency = 5;
 
     // Start is called before the first frame update
     private void Start()
     {
-        _pool = new ObjectPool<Trash>(() => Instantiate(trashPrefab, GetSpawnLocation(), Random.rotation), trash =>
-        {
-            trash.gameObject.SetActive(true);
-        }, trash =>
-        {
-            trash.gameObject.SetActive(false);
-        }, trash =>
-        {
-            Destroy(trash.gameObject);
-        }, false, trashCount, maxTrash);
-
         InvokeRepeating(nameof(Spawn), 0.1f, spawnFrequency);
     }
 
-    private void Update()
-    {
-        print(_pool.CountActive);
-    }
+
 
     private void Spawn()
     {
-        var shape = _pool.Get();
-        shape.Init(KillTrash);
+        Instantiate(trashPrefab, GetSpawnLocation(), transform.rotation);
     }
 
     private Vector3 GetSpawnLocation()
@@ -54,8 +35,5 @@ public class TrashSpawner : MonoBehaviour
         return spawnPoints[i].returnPosition();
     }
 
-    private void KillTrash(Trash trash)
-    {
-        _pool.Release(trash);
-    }
+
 }
